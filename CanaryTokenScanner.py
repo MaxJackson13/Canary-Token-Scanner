f29b95c75089a9b173f6955b9583cda8ef707c8f
MaxJackson13 <72030655+MaxJackson13@users.noreply.github.com>
2022-11-23 15:50:22 +0000

    Update CanaryTokenScanner.py

--- CanaryTokenScanner.py
+++ CanaryTokenScanner.py
@@ -12,20 +12,21 @@ options = parser.parse_args()
 url = options.url
 target = options.target
 
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
-scripts = soup.findAll('script')
+response = requests.get(url)                                                                                # Retrieve contents from the url
+soup = BeautifulSoup(response.text, "html.parser")                                                          # Parse the html so we can extract data by tag
+scripts = soup.findAll('script')                                                                            # Retrieve all script tags
 
-matches = ['http://canarytokens.com/', target]
-
+matches = ['http://canarytokens.com/', target]                                                              # Strings to match in the canary token
+encoded_matches = ['\\x'+"\\x".join([hex(ord(char))[-2:] for char in match]) for match in matches]          # Hex encode the strings to match on 
+                                                                                                            # obfuscated javascript
 token = False
 for i,script in enumerate(scripts):
-    if all([x in script.text for x in matches]):
-        print('Token found: '+ '\n' + scripts[i].text)
+    if all([x in script.text for x in matches]) or all([x in script.text for x in encoded_matches]):        # Check for both the strings in the same script
+        print('Token found: '+ '\n' + scripts[i].text)                                                      # block, separating by encoded and plaintext
         token = True
-        break
+        break                                                                                               # Break once we've found a token and return it
     else:
         pass
 
 if not token:
-    print('Token not found')
+    print('Token not found')                                                                                # Tell us if no token found 
