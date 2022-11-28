67a9be8ef18d2f26b58a7c0d7fa4590716f79d80
MaxJackson13 <72030655+MaxJackson13@users.noreply.github.com>
2022-11-28 02:36:11 +0000

    Create WordCanaryTokenScanner.py

---
+++ WordCanaryTokenScanner.py
@@ -0,0 +1,32 @@
+from zipfile import ZipFile
+import xml.etree.ElementTree as et
+import re
+
+zipfile = ZipFile('Downloads/canary.docx')
+files = zipfile.namelist()
+rels = [rel for rel in files if rel.endswith('.rels')]
+
+pattern = re.compile(r"^https?://canarytokens.com/.*(/contact.php)$")
+
+tokens = []
+
+for rel in rels:
+    
+    file = zipfile.open(rel, 'r')
+    
+    tree = et.parse(file)
+    root = tree.getroot()
+    
+    for child in root:
+        
+        if 'TargetMode' in child.attrib.keys() and child.attrib['TargetMode'] == 'External':
+            
+            target = child.attrib['Target']
+            typ = child.attrib['Type']
+            
+            if pattern.search(target):
+                tokens.append(pattern.search(target).group(0))
+            
+            print('External link found: ' + target + '\nType: ' + typ.split('/')[-1] + '\n')
+
+print('\n'.join(tokens))
